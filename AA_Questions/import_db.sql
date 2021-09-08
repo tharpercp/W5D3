9c7d143f52a60197449f0c7b69d4b97b4349fd24
icidij <jamesc1288@gmail.com>
2021-09-08 14:28:02 -0400

    update foreign key syntax

--- AA_Questions/import_db.sql
+++ AA_Questions/import_db.sql
@@ -3,30 +3,38 @@ PRAGMA foreign_keys = ON;
 
 
 CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
     fname VARCHAR,
     lname VARCHAR
 );
 
 CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
     title VARCHAR,
     body VARCHAR,
 
-    author PRIMARY KEY
+    FOREIGN KEY author VARCHAR REFERENCES question_follows(user_id) 
 );
 
 CREATE TABLE question_follows (
-    question_id PRIMARY KEY,
-    user_id INTEGER NOT NULL
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 CREATE TABLE replies (
-
+    parent_reply INTEGER PRIMARY KEY,
+    child_reply INTEGER, 
     q_id integer NOT NULL,
     normal_id integer NOT NULL,
 
     u_id integer NOT NULL,
 
-    r_body VARCHAR
-    
+    r_body VARCHAR,
+    FOREIGN KEY (q_id) REFERENCES questions(id),
+    FOREIGN KEY (u_id) REFERENCES users(id),
+    FOREIGN KEY (child_reply) REFERENCES replies(parent_reply)
 );
 
 CREATE TABLE question_likes (
@@ -35,21 +43,23 @@ CREATE TABLE question_likes (
     likes integer,
 
     id_u integer,
-    id_q integer 
+    id_q integer,
+    FOREIGN KEY (id_u) REFERENCES users(id),
 
+    FOREIGN KEY (id_q) REFERENCES questions(id)
 );
 
-INSERT INTO users (fname, lname)
-VALUES ("John", "Doe");
+-- INSERT INTO users (fname, lname)
+-- VALUES ("John", "Doe");
 
-INSERT INTO users (fname, lname)
-VALUES ("Jane", "Doe");
+-- INSERT INTO users (fname, lname)
+-- VALUES ("Jane", "Doe");
 
-INSERT INTO questions (title, body)
-VALUES ("AA_Question", "What is coding?");
+-- INSERT INTO questions (title, body)
+-- VALUES ("AA_Question", "What is coding?");
 
-INSERT INTO replies (q_id, normal_id, u_id, r_body)
-VALUES ( 1, 1, 1, "Coding is fun!");
+-- INSERT INTO replies (q_id, normal_id, u_id, r_body)
+-- VALUES ( 1, 1, 1, "Coding is fun!");
 
-INSERT INTO question_likes(likes, id_u, id_q)
-VALUES (1000000, 2, 2);
+-- INSERT INTO question_likes(likes, id_u, id_q)
+-- VALUES (1000000, 2, 2);
